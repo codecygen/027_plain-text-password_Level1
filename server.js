5d16b25e571364451e4c79d5323450332326b6d3
gingercook <vahitaras@gmail.com>
2021-10-15 00:42:20 -0400

    mongoose added. post request for register page created on server.js

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
 //jshint esversion:6
 // Load Node modules
-let express = require('express');
+const express = require('express');
 const ejs = require('ejs');
 // Initialize Express
-let app = express();
+const app = express();
 
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 app.use(bodyParser.urlencoded({extended: true}));
 
 // Render static files
 app.use(express.static(__dirname + '/public'));
 // Set the view engine to ejs
 app.set('view engine', 'ejs');
+
+async function main() {
+    await mongoose.connect('mongodb://localhost:27017/userDB', { useNewUrlParser: true });
+}
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+const User = mongoose.model('User', userSchema);
 
 
 app.get('/', (req, res) => {
@@ -26,7 +38,10 @@ app.get('/register', (req, res) => {
     res.render('register');
 });
 
-
+app.post('/register', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+});
 
 
 // Port website will run on
